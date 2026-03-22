297d329f2f1747612ffc95a394b9f6fb1fe43ce3
AndresHGdeG <ah.mx@outlook.com>
2026-03-21 20:07:09 -0600

    deliting icon script

--- run-make-icon.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH=/home/as-dev/.nvm/versions/node/v18.20.8/bin:/usr/local/bin:/usr/bin:/bin
-cd /home/as-dev/repositories/es-navigator-app
-npx @bam.tech/react-native-make set-icon --path ./assets/refitapp-logo.png
